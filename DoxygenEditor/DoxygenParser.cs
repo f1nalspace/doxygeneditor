b3a4d1517e8f3a8a4443fab813303c53d4b06cbb
Finalspace <finalspace@googlemail.com>
2018-11-24 10:46:54 +0100

    Initial commit

---
+++ DoxygenEditor/DoxygenParser.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoxygenEditor
+{
+    public class DoxygenParser
+    {
+        public class SourceLineInfo
+        {
+            public int LineIndex = 0;
+            public int LineStartPos = 0;
+            public int LineLength = 0;
+            public SourceLineInfo()
+            {
+            }
+        }
+
+        public class LineState
+        {
+            public SourceLineInfo Info { get; }
+            public string Value { get; }
+            public int Offset { get; private set; }
+            public int Length { get; set; }
+            public char CurrentChar
+            {
+                get
+                {
+                    return Value[Offset];
+                }
+            }
+            public bool IsEndOfLine
+            {
+                get
+                {
+                    bool result = !(Offset < Length);
+                    return (result);
+                }
+            }
+            public string RemainingValue
+            {
+                get
+                {
+                    int len = Length - Offset;
+                    string result = Value.Substring(Offset, len);
+                    return (result);
+                }
+            }
+            public LineState(SourceLineInfo info, string value)
+            {
+                Info = info;
+                Value = value;
+                Offset = 0;
+                Length = value.Length;
+            }
+            public void IncOffset(int addon = 1)
+            {
+                if (Offset < Length - (addon - 1))
+                    Offset += addon;
+                else
+                    throw new InvalidOperationException("End of line reached!");
+            }
+            public override string ToString()
+            {
+                return Value;
+            }
+        }
+
+        public abstract class Entity
+        {
+            public Entity Parent { get; protected set; }
+            private readonly List<Entity> _children = new List<Entity>();
+            public IEnumerable<Entity> Children { get { return _children; } }
+            public SourceLineInfo LineInfo { get; }
+            public Entity(SourceLineInfo lineInfo)
+            {
+                LineInfo = lineInfo;
+                Parent = null;
+            }
+            public void AddChild(Entity child)
+            {
+                child.Parent = this;
+                _children.Add(child);
+            }
+        }
+
+        public class PageEntity : Entity
+        {
+            public string PageId { get; }
+            public string PageCaption { get; }
+            public PageEntity(SourceLineInfo lineInfo, string pageId, string pageCaption) : base(lineInfo)
+            {
+                PageId = pageId;
+                PageCaption = pageCaption;
+            }
+            public override string ToString()
+            {
+                return $"{PageId} {PageCaption}";
+            }
+        }
+        public class MainPageEntity : PageEntity
+        {
+            public MainPageEntity(SourceLineInfo lineInfo) : base(lineInfo, null, "MainPage") { }
+        }
+
+        public class RootEntity : Entity
+        {
+            public RootEntity() : base(null)
+            {
+
+            }
+        }
+
+        public class SectionEntity : Entity
+        {
+            public string SectionId { get; }
+            public string SectionCaption { get; }
+            public SectionEntity(SourceLineInfo lineInfo, string id, string caption) : base(lineInfo)
+            {
+                SectionId = id;
+                SectionCaption = caption;
+            }
+        }
+
+        public class SubSectionEntity : SectionEntity
+        {
+            public SubSectionEntity(SourceLineInfo lineInfo, string id, string caption) : base(lineInfo, id, caption)
+            {
+            }
+        }
+
+        public enum MessageType
+        {
+            Error = 0,
+            Warning,
+        }
+
+        public class Message
+        {
+            public MessageType Type { get; }
+            public string Text { get; }
+            public SourceLineInfo LineInfo { get; }
+
+            public Message(MessageType type, string text, SourceLineInfo lineInfo)
+            {
+                Type = type;
+                Text = text;
+                LineInfo = lineInfo;
+            }
+
+            public override string ToString()
+            {
+                return $"{Type}[{LineInfo.LineIndex}:{LineInfo.LineStartPos}] {Text}";
+            }
+        }
+
+        public class Context
+        {
+            public string SourceText { get; }
+            public int LinePos { get; private set; } = 0;
+            public int LineCount { get { return LineInfos.Count; } }
+            public readonly List<SourceLineInfo> LineInfos = new List<SourceLineInfo>();
+            private readonly Stack<Entity> _entityStack = new Stack<Entity>();
+            public readonly RootEntity RootEntity = new RootEntity();
+
+            private readonly List<Message> _messages = new List<Message>();
+            public IEnumerable<Message> Messages { get { return _messages; } }
+
+            public Context(string sourceText)
+            {
+                SourceText = sourceText;
+            }
+
+            public Entity Top
+            {
+                get
+                {
+                    if (_entityStack.Count > 0)
+                        return _entityStack.Peek();
+                    return null;
+                }
+            }
+
+            public void AddPage(Entity pageEntity)
+            {
+                _entityStack.Clear();
+                _entityStack.Push(pageEntity);
+                RootEntity.AddChild(pageEntity);
+            }
+
+            private void ClearUntilType(Type type)
+            {
+                while (_entityStack.Count > 0)
+                {
+                    Entity p = _entityStack.Peek();
+                    if (!type.IsInstanceOfType(p))
+                        _entityStack.Pop();
+                    else
+                        break;
+                }
+            }
+
+            public void AddSection(Entity sectionEntity)
+            {
+                if (typeof(SectionEntity).Equals(sectionEntity.GetType()))
+                    ClearUntilType(typeof(PageEntity));
+                else
+                {
+                    Debug.Assert(typeof(SubSectionEntity).Equals(sectionEntity.GetType()));
+                    ClearUntilType(typeof(SectionEntity));
+                }
+                if (_entityStack.Count > 0)
+                {
+                    Entity pageEntity = _entityStack.Peek();
+                    pageEntity.AddChild(sectionEntity);
+                    _entityStack.Push(sectionEntity);
+                }
+                else
+                    AddMessage(MessageType.Error, sectionEntity.LineInfo, $"Section '{sectionEntity}' must be parented to a page!");
+            }
+
+            public void Push(Entity entity)
+            {
+                _entityStack.Push(entity);
+            }
+            public Entity Pop()
+            {
+                Debug.Assert(_entityStack.Count > 0);
+                Entity result = _entityStack.Pop();
+                return (result);
+            }
+
+            public void NextLine()
+            {
+                if (LinePos < LineCount)
+                    ++LinePos;
+                else
+                    throw new InvalidOperationException("All lines are already consumed!");
+            }
+
+            public LineState GetCurrentLine()
+            {
+                if (LinePos < LineCount)
+                {
+                    SourceLineInfo lineInfo = LineInfos[LinePos];
+                    string value = SourceText.Substring(lineInfo.LineStartPos, lineInfo.LineLength);
+                    LineState result = new LineState(lineInfo, value);
+                    return (result);
+                }
+                else
+                    throw new InvalidOperationException("All lines are already consumed!");
+            }
+
+            public void AddMessage(MessageType type, SourceLineInfo lineInfo, string text)
+            {
+                _messages.Add(new Message(type, text, lineInfo));
+            }
+        }
+
+        private void SkipWhitespaces(ref LineState line)
+        {
+            while (!line.IsEndOfLine && (char.IsWhiteSpace(line.CurrentChar)))
+            {
+                line.IncOffset();
+            }
+        }
+
+        private string ParseIdentifierString(ref LineState line)
+        {
+            if (!line.IsEndOfLine && (char.IsLetter(line.CurrentChar) || line.CurrentChar == '_'))
+            {
+                string result = "";
+                while (!line.IsEndOfLine && (char.IsLetterOrDigit(line.CurrentChar) || line.CurrentChar == '_'))
+                {
+                    result += line.CurrentChar;
+                    line.IncOffset();
+                }
+                return (result);
+            }
+            return null;
+        }
+        private Keyword ParseIdentifierKeyword(ref LineState line)
+        {
+            if (!line.IsEndOfLine && (char.IsLetter(line.CurrentChar) || line.CurrentChar == '_'))
+            {
+                Keyword result = new Keyword() { Start = line.Offset };
+                string ident = ParseIdentifierString(ref line);
+                result.Length = line.Offset - result.Start;
+                result.Value = ident;
+                return (result);
+            }
+            return null;
+        }
+        private Keyword ParseRemainingKeyword(ref LineState line)
+        {
+            Keyword result = new Keyword() { Start = line.Offset };
+            result.Length = line.Length - line.Offset;
+            result.Value = line.Value.Substring(line.Offset, result.Length);
+            line.IncOffset(result.Length);
+            return (result);
+        }
+        class Keyword
+        {
+            public int Start;
+            public int Length;
+            public string Value;
+        }
+
+        private Keyword ParseCommand(ref LineState line)
+        {
+            Debug.Assert(line.CurrentChar == '@');
+            line.IncOffset();
+            Keyword result = ParseIdentifierKeyword(ref line);
+            return (result);
+        }
+
+        private void ParsePage(Context state, ref LineState line, string pageCommand)
+        {
+            SkipWhitespaces(ref line);
+            PageEntity pageEntity;
+            if ("page".Equals(pageCommand))
+            {
+                // Identifier
+                Keyword pageIdentKeyword = ParseIdentifierKeyword(ref line);
+                if (pageIdentKeyword == null)
+                {
+                    state.AddMessage(MessageType.Warning, line.Info, "Page requires a identifier!");
+                    return;
+                }
+                string pageIdent = pageIdentKeyword.Value;
+
+                // Caption
+                SkipWhitespaces(ref line);
+                Keyword pageCaptionKeyword = ParseRemainingKeyword(ref line);
+                string pageCaption = pageCaptionKeyword.Value;
+                pageEntity = new PageEntity(line.Info, pageIdent, pageCaption);
+            }
+            else
+            {
+                // Main Page
+                Debug.Assert("mainpage".Equals(pageCommand));
+                pageEntity = new MainPageEntity(line.Info);
+            }
+            state.AddPage(pageEntity);
+        }
+
+        private void ParseSection(Context state, ref LineState line, string sectionCommand)
+        {
+            // Identifier
+            SkipWhitespaces(ref line);
+            Keyword sectionIdentKeyword = ParseIdentifierKeyword(ref line);
+            if (sectionIdentKeyword == null) return;
+            string sectionIdent = sectionIdentKeyword.Value;
+
+            // Caption
+            string sectionCaption = null;
+            if (!line.IsEndOfLine)
+            {
+                SkipWhitespaces(ref line);
+                Keyword sectionCaptionKeyword = ParseRemainingKeyword(ref line);
+                if (sectionCaptionKeyword != null)
+                    sectionCaption = sectionCaptionKeyword.Value;
+            }
+
+            SectionEntity sectionEntity;
+            if ("section".Equals(sectionCommand))
+                sectionEntity = new SectionEntity(line.Info, sectionIdent, sectionCaption);
+            else
+            {
+                Debug.Assert("subsection".Equals(sectionCommand));
+                sectionEntity = new SubSectionEntity(line.Info, sectionIdent, sectionCaption);
+            }
+            state.AddSection(sectionEntity);
+        }
+
+        public Context Parse(string sourceText)
+        {
+            //
+            // 1.) Parse line infos (Start and length for each line)
+            //
+            Context result = new Context(sourceText);
+            StringBuilder lineBuffer = new StringBuilder();
+            int sourceCodeLen = sourceText.Length;
+            int sourceCodePos = 0;
+            int lineIndex = 0;
+            SourceLineInfo currentLineInfo = new SourceLineInfo();
+            while (sourceCodePos < sourceCodeLen)
+            {
+                char c = sourceText[sourceCodePos];
+                if (c == '\n')
+                {
+                    currentLineInfo.LineLength = lineBuffer.Length;
+                    currentLineInfo.LineIndex = lineIndex;
+                    result.LineInfos.Add(currentLineInfo);
+
+                    currentLineInfo = new SourceLineInfo();
+                    currentLineInfo.LineStartPos = sourceCodePos + 1;
+
+                    lineBuffer.Clear();
+                    ++lineIndex;
+                }
+                else
+                    lineBuffer.Append(c);
+                ++sourceCodePos;
+            }
+
+            //
+            // 2.) Parse each line
+            //
+            Entity rootEntity = new RootEntity();
+
+            bool insideDoxygenComment = false;
+            while (result.LinePos < result.LineCount)
+            {
+                SourceLineInfo lineInfo = result.LineInfos[result.LinePos];
+                string line = sourceText.Substring(lineInfo.LineStartPos, lineInfo.LineLength);
+                if (!insideDoxygenComment)
+                {
+                    if (line.TrimStart().StartsWith("/*!"))
+                        insideDoxygenComment = true;
+                    result.NextLine();
+                    continue;
+                }
+                if (line.TrimStart().StartsWith("*/"))
+                {
+                    insideDoxygenComment = false;
+                    result.NextLine();
+                    continue;
+                }
+                Debug.Assert(insideDoxygenComment == true);
+                LineState pageLine = result.GetCurrentLine();
+                SkipWhitespaces(ref pageLine);
+                if (!pageLine.IsEndOfLine)
+                {
+                    if ('@'.Equals(pageLine.CurrentChar))
+                    {
+                        // Command
+                        SkipWhitespaces(ref pageLine);
+                        Keyword keyword = ParseCommand(ref pageLine);
+                        if (keyword != null)
+                        {
+                            if ("mainpage".Equals(keyword.Value) || "page".Equals(keyword.Value))
+                                ParsePage(result, ref pageLine, keyword.Value);
+                            else if ("section".Equals(keyword.Value) || "subsection".Equals(keyword.Value))
+                                ParseSection(result, ref pageLine, keyword.Value);
+                        }
+                    }
+                }
+                result.NextLine();
+            }
+            return (result);
+        }
+    }
+}
---
+++ DoxygenEditor/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace DoxygenEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tvTree = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileOpenMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 482);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(833, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tvTree);
+            this.splitContainer1.Size = new System.Drawing.Size(833, 458);
+            this.splitContainer1.SplitterDistance = 277;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // tvTree
+            // 
+            this.tvTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvTree.Location = new System.Drawing.Point(0, 0);
+            this.tvTree.Name = "tvTree";
+            this.tvTree.Size = new System.Drawing.Size(277, 458);
+            this.tvTree.TabIndex = 0;
+            this.tvTree.TabStop = false;
+            this.tvTree.DoubleClick += new System.EventHandler(this.tvTree_DoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(833, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileOpenMenuItem,
+            this.toolStripMenuItem1,
+            this.exitMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // fileOpenMenuItem
+            // 
+            this.fileOpenMenuItem.Name = "fileOpenMenuItem";
+            this.fileOpenMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.fileOpenMenuItem.Text = "Open...";
+            this.fileOpenMenuItem.Click += new System.EventHandler(this.fileOpenMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(109, 6);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitMenuItem.Text = "Exit";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 504);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "DoxygenEditor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView tvTree;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileOpenMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+    }
+}
+
