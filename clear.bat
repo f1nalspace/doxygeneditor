a2a0479a42e72f6dd40646d215e5111b1de89a80
Finalspace <finalspace@googlemail.com>
2019-03-05 18:56:09 +0100

    Updated solution

--- clear.bat
+++ clear.bat
@@ -6,4 +6,13 @@ rmdir /q /s .vs
 rmdir /q /s build
 rmdir /q /s setup
 rmdir /q /s packages
+rmdir /q /s Debug
+rmdir /q /s Release
+rmdir /q /s x64
+rmdir /q /s DoxygenEditor\bin
 rmdir /q /s DoxygenEditor\obj
+rmdir /q /s DoxygenParsers\bin
+rmdir /q /s DoxygenParsers\obj
+rmdir /q /s FastTokenizer\bin
+rmdir /q /s FastTokenizer\obj
+rmdir /q /s CParserTest\x64
