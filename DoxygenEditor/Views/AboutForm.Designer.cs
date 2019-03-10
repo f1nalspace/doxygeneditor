50b638a84b769fe5cabd16451ce4aadb277c273c
Finalspace <finalspace@googlemail.com>
2019-03-10 16:41:50 +0100

    Styled about dialog

--- DoxygenEditor/Views/AboutForm.Designer.cs
+++ DoxygenEditor/Views/AboutForm.Designer.cs
@@ -43,17 +43,19 @@
             // 
             this.panBottom.Controls.Add(this.btnOk);
             this.panBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panBottom.Location = new System.Drawing.Point(0, 328);
+            this.panBottom.Location = new System.Drawing.Point(0, 258);
+            this.panBottom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panBottom.Name = "panBottom";
-            this.panBottom.Size = new System.Drawing.Size(732, 36);
+            this.panBottom.Size = new System.Drawing.Size(549, 38);
             this.panBottom.TabIndex = 0;
             // 
             // btnOk
             // 
             this.btnOk.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnOk.Location = new System.Drawing.Point(329, 6);
+            this.btnOk.Location = new System.Drawing.Point(201, 4);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.Size = new System.Drawing.Size(142, 30);
             this.btnOk.TabIndex = 0;
             this.btnOk.Text = "OK";
             this.btnOk.UseVisualStyleBackColor = true;
@@ -67,8 +69,9 @@
             this.panClient.Controls.Add(this.labelAppName);
             this.panClient.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panClient.Location = new System.Drawing.Point(0, 0);
+            this.panClient.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panClient.Name = "panClient";
-            this.panClient.Size = new System.Drawing.Size(732, 328);
+            this.panClient.Size = new System.Drawing.Size(549, 258);
             this.panClient.TabIndex = 1;
             // 
             // tbLicense
@@ -78,12 +81,13 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbLicense.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbLicense.Font = new System.Drawing.Font("Lucida Sans Unicode", 10.2F);
-            this.tbLicense.Location = new System.Drawing.Point(3, 137);
+            this.tbLicense.Location = new System.Drawing.Point(2, 111);
+            this.tbLicense.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbLicense.Multiline = true;
             this.tbLicense.Name = "tbLicense";
             this.tbLicense.ReadOnly = true;
             this.tbLicense.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbLicense.Size = new System.Drawing.Size(726, 188);
+            this.tbLicense.Size = new System.Drawing.Size(544, 145);
             this.tbLicense.TabIndex = 4;
             this.tbLicense.WordWrap = false;
             // 
@@ -91,9 +95,10 @@
             // 
             this.labelDescription.AutoSize = true;
             this.labelDescription.Font = new System.Drawing.Font("Lucida Sans Unicode", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDescription.Location = new System.Drawing.Point(8, 98);
+            this.labelDescription.Location = new System.Drawing.Point(6, 80);
+            this.labelDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelDescription.Name = "labelDescription";
-            this.labelDescription.Size = new System.Drawing.Size(117, 21);
+            this.labelDescription.Size = new System.Drawing.Size(96, 17);
             this.labelDescription.TabIndex = 3;
             this.labelDescription.Text = "[Description]";
             // 
@@ -101,9 +106,10 @@
             // 
             this.labelAppVersion.AutoSize = true;
             this.labelAppVersion.Font = new System.Drawing.Font("Lucida Sans Unicode", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAppVersion.Location = new System.Drawing.Point(7, 62);
+            this.labelAppVersion.Location = new System.Drawing.Point(5, 50);
+            this.labelAppVersion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelAppVersion.Name = "labelAppVersion";
-            this.labelAppVersion.Size = new System.Drawing.Size(118, 21);
+            this.labelAppVersion.Size = new System.Drawing.Size(98, 17);
             this.labelAppVersion.TabIndex = 1;
             this.labelAppVersion.Text = "[AppVersion]";
             // 
@@ -111,20 +117,22 @@
             // 
             this.labelAppName.AutoSize = true;
             this.labelAppName.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAppName.Location = new System.Drawing.Point(5, 15);
+            this.labelAppName.Location = new System.Drawing.Point(4, 12);
+            this.labelAppName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelAppName.Name = "labelAppName";
-            this.labelAppName.Size = new System.Drawing.Size(166, 34);
+            this.labelAppName.Size = new System.Drawing.Size(130, 27);
             this.labelAppName.TabIndex = 0;
             this.labelAppName.Text = "[AppName]";
             // 
             // AboutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 364);
+            this.ClientSize = new System.Drawing.Size(549, 296);
             this.Controls.Add(this.panClient);
             this.Controls.Add(this.panBottom);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
