6e29073f947bc4b0fb612bf5b6b2ee7a0424463b
f1nalspace <finalspace@googlemail.com>
2022-11-25 11:59:41 +0100

    Added benchmarks project

---
+++ Benchmarks/SimpleSymbolTableId.cs
@@ -0,0 +1,14 @@
+﻿using TSP.DoxygenEditor.Symbols;
+
+namespace Benchmarks
+{
+    class SimpleSymbolTableId : ISymbolTableId
+    {
+        public object SymbolTableId { get; }
+
+        public SimpleSymbolTableId(int id)
+        {
+            SymbolTableId = id;
+        }
+    }
+}
